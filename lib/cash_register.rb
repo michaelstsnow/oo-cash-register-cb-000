a6cc44d39fe2b2c591b5b79c0d6d46ba474cb156
michaelstsnow <michael.st.snow@gmail.com>
2018-11-22 20:59:46 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   def add_item(item,price,quantity=1)
       added_value=price * quantity
       @total += added_value
-      while quantity >=1 
+      while quantity >=1
         @items << item
         quantity -= 1
       end
