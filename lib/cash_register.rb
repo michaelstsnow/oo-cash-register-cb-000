216189ded875329b123aec949837ab3ecd63bd61
michaelstsnow <michael.st.snow@gmail.com>
2018-11-22 21:13:43 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
     @total = @total - ((@discount/100) * @total)
     total_string=@total.to_s.split(".")
     if @discount > 0
-      "After the discount, the total comes to $#{total_string}."
+      "After the discount, the total comes to $#{total_string[0]}."
     else
       "There is no discount to apply."
     end
