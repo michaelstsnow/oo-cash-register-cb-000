6dd1946d26be7fcfb23f0b0344b186767dceded8
michaelstsnow <michael.st.snow@gmail.com>
2018-11-22 21:38:38 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -31,8 +31,8 @@ class CashRegister
   end
 
   def void_last_transaction
-    quantity_length=@quantities.length
-    this_quantity=@quantities[quantity_length-1]
+    quantity_length=@quantities.length - 1
+    this_quantity=@quantities[quantity_length]
     @total -= @price[@quantities.length-1] * this_quantity
 
     @items.pop(@quantities.[quantity_length-1])
