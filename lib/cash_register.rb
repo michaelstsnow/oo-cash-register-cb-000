f40b9b81a92b009a8b4ef2e9d6b735371f057a87
michaelstsnow <michael.st.snow@gmail.com>
2018-11-22 21:07:35 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,7 +18,7 @@ class CashRegister
 
   def apply_discount
     #@total = @total - ((@discount/100) * @total)
-    @total = (@discount/100)
+    @total = (@discount.to_i/100)
     if @discount > 0
       "After the discount, the total comes to $#{@total}."
     else
